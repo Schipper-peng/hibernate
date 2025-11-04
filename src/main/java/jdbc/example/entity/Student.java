8250cd5943902d4eae58d293e02dbcd35e2c63ff
Schipper <havkinscy@mail.ru>
2025-11-04 05:08:29 +0300

    JPA

---
+++ src/main/java/jdbc/example/entity/Student.java
@@ -0,0 +1,84 @@
+package jdbc.example.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "avg_grade")
+    private Double avgGrade;
+    public Student() {
+
+    }
+
+    public Student(String name, String surname, Double avgGrade) {
+        this.name = name;
+        this.surname = surname;
+        this.avgGrade = avgGrade;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(Double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(surname, student.surname) && Objects.equals(avgGrade, student.avgGrade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, avgGrade);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
+
