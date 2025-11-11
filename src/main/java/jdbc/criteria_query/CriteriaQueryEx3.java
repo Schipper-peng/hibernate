406a67faa7e1aa2599666e7974b9ede37272c9d9
Schipper-peng <havkinscy@mail.ru>
2025-11-11 22:42:08 +0300

    new

--- src/main/java/jdbc/criteria_query/CriteriaQueryEx3.java
+++ src/main/java/jdbc/criteria_query/CriteriaQueryEx3.java
@@ -1,15 +1,13 @@
 package jdbc.criteria_query;
 
 import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import jdbc.criteria_query.entity.Student;
+import jdbc.criteria_query.entity.University;
 
 import java.util.List;
 
-public class CriteriaQueryEx2 {
+public class CriteriaQueryEx3 {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence
                 .createEntityManagerFactory("jpa-course");
@@ -20,40 +18,44 @@ public class CriteriaQueryEx2 {
         try {
             transaction.begin();
 
-//            JPQL: select s.name from Student;\
+//            *******************************************************************************************************
+            //            JPQL: select s.name and s.avgGrade     from Student s
 //            1 Creation of Criteria Builder
 //            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 //            2 Creation of Criteria Query
-//            CriteriaQuery<String> criteriaQuery = criteriaBuilder.createQuery(String.class);
+//            CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
 //            3 Root creation
 //            Root<Student> root = criteriaQuery.from(Student.class);//from Student s
 //            4 Adding rot to Criteria Query
-//            criteriaQuery.select(root.get("name")); // select s from Student s
+//            criteriaQuery.multiselect(root.get("name"), root.get("avgGrade")); // select s.name. s.avgGrade from Student s
 //            5 Query creation
-//            TypedQuery<String> query = entityManager.createQuery(criteriaQuery);
-//            List<String> names = query.getResultList();
-//            System.out.println(names);
-//            Query query100 = entityManager.createQuery("select s from Student");
+//            TypedQuery<Object[]> query = entityManager.createQuery(criteriaQuery);
+//            List<Object[]> studentInfo = query.getResultList();
+//            for(Object[] info : studentInfo) {
+//                System.out.println(info[0] + " " + info[1]);
+//            }
 //            *******************************************************************************************************
-
-            //            JPQL: select s from Student s where avgGrade >= 7.5;
+            //            JPQL: select u, s from University u JOIN u.student s;
 //            1 Creation of Criteria Builder
             CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 //            2 Creation of Criteria Query
-            CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
+            CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
 //            3 Root creation
-            Root<Student> root = criteriaQuery.from(Student.class);//from Student s
-            //3.1 Condition creation
-            Predicate predicate = criteriaBuilder.greaterThanOrEqualTo(root.get("avgGrade"), 7.5);
-            //3.2 Adding condition to Criteria query
-            criteriaQuery.where(predicate);
+            Root<University> root = criteriaQuery.from(University.class);//from University u
+//            3.1 JOIN
+            Join<University, Student> join = root.join("students");
 //            4 Adding root to Criteria Query
-            criteriaQuery.select(root); // select s from Student s where s.avgGrade >= 7.5
+            criteriaQuery.multiselect(root, join); // select u, s from University u JOIN // u.students s
 //            5 Query creation
-            TypedQuery<Student> query = entityManager.createQuery(criteriaQuery);
-            List<Student> names = query.getResultList();
-            System.out.println(names);
+            TypedQuery<Object[]> query = entityManager.createQuery(criteriaQuery);
+            List<Object[]> students = query.getResultList();
+            for(Object[] info : students) {
+                System.out.println(info[0] + "-->" + info[1]);
+            }
 //            *******************************************************************************************************
+
+
+
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
